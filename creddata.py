05c2345506e3baff4d76e2bb1233a460c3a3a9f5
Kiptim <kiptim54@gmail.com>
2018-03-29 13:32:32 +0300

    add test for save credentials

--- creddata.py
+++ creddata.py
@@ -4,6 +4,13 @@ class Credentials:
     '''
     cred_list = []
 
+    def save_cred(self):
+        '''
+        self credentials in cred_list
+        '''
+        Credentials.cred_list.append(self)
+
+
     def __init__(self, account , username , password):
 
         self.account = account
--- creddata.test.py
+++ creddata.test.py
@@ -1,2 +1,28 @@
 import unittest
 from creddata import Credentials
+
+class TestCredentials(unittest.TestCase):
+
+    def setUp(self):
+        '''
+        setup before a test is run
+        '''
+        self.new_cred = Credentials("Facebook", "Kiptim54", "Author7")
+    def test_init(self):
+        '''
+        check if instances initialize as expected
+        '''
+        self.assertEqual(self.new_cred.account, "Facebook")
+        self.assertEqual(self.new_cred.username, "Kiptim54")
+        self.assertEqual(self.new_cred.password, "Author7")
+    def test_save_credentials(self):
+        '''
+        check if credentials can be saved
+        '''  
+        self.new_cred.save_cred()
+        self.assertEqual(len(Credentials.cred_list),1)
+        
+    
+
+if __name__ == '__main__':
+    unittest.main()
